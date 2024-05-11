d6b7b7d5e4c9211b18091718ad4c9efe7b225af1
SebaschaM <schaquila@autonoma.edu.pe>
2024-05-11 02:36:24 -0500

    Refactor API base URL to use production server

--- src/api/karnavalApi.tsx
+++ src/api/karnavalApi.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const karnavalApi = axios.create({
-  //  baseURL: "https://karnaval-plaza-backend-production.up.railway.app/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://karnaval-plaza-backend-production.up.railway.app/api",
+  //baseURL: "http://localhost:3000/api",
 });
